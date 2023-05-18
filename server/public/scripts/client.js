dff99373018d6868ef5c10a85aae2634c992ec9a
Will Richardson <rich1051@umn.edu>
2023-05-18 15:31:22 -0500

    added postKoalas function, fixed add button on click, updated onReady function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,31 +1,33 @@
 console.log( 'js' );
 
-$( document ).ready( function(){
+$(document).ready(onReady);
+
+function onReady() {
   console.log( 'JQ' );
   // Establish Click Listeners
-  setupClickListeners()
   // load existing koalas on page load
   getKoalas();
 
-}); // end doc ready
+  $('#addButton').on('click', postKoalas);
+}; // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
+function setupClickListeners() {}
+  // $( '#addButton' ).on( 'click', postKoalas(){
+  //   console.log( 'in addButton on click' );
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
-}
+//     let koalaToSend = {
+//       name: 'testName',
+//       age: 'testName',
+//       gender: 'testName',
+//       readyForTransfer: 'testName',
+//       notes: 'testName',
+//     };
+//     // call saveKoala with the new obejct
+//     saveKoala( koalaToSend );
+//   }); 
+// }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
@@ -38,3 +40,82 @@ function saveKoala( newKoala ){
   // ajax call to server to get koalas
  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function postKoalas() {
+  let koalaToSend = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#genderIn').val(),
+    readyForTransfer: $('#readyForTransferIn').val(),
+    notes: $('#notesIn').val()
+  };
+  $.ajax({
+      type: 'POST',
+      url: '/koalas',
+      data: koalaToSend
+  }).then(function(response) {
+    $('#nameIn').val(''),
+    $('#ageIn').val(''),
+    $('#genderIn').val(''),
+    $('#readyForTransferIn').val(''),
+    $('#notesIn').val('')
+      getKoalas();
+  });
+};
