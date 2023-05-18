1da4da39cddf36c43b582c88d97b14a5e8b1a7ff
Will Richardson <rich1051@umn.edu>
2023-05-18 15:53:34 -0500

    router post function created

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -7,8 +7,119 @@ const koalaRouter = express.Router();
 // GET
 
 
-// POST
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// POST
+router.post('/', (req, res) => {
+        let newKoala = req.body;
+        console.log('req.body:', req.body);
+    // parameterized query:
+        let queryText = `
+        INSERT INTO "Koala_Bears" ("name", "age", "gender", "ready for transfer", "notes")
+        VALUES ($1, $2, $3, $4, $5);
+        `;
+        
+    // these values are substituted for $1, $2, $3, $4, $5
+        let values = [newKoala.name, newKoala.age, newKoala.gender, newKoala.readyForTransfer, newKoala.notes];
+    
+    // pass queryText and array of values
+        pool.query(queryText, values)
+            .then(result => {
+                res.sendStatus(201);
+            })
+            .catch(error => {
+                console.log('Query:', queryText, 'Error:', error);
+                res.sendStatus(500);
+            })
+    });
 
 // PUT
 
